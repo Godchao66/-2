960a1e9cb10f46bc5b79396b3933123510416d28
Godchao66 <90696318+Godchao66@users.noreply.github.com>
2021-09-16 20:11:58 +0800

    2
    2

---
+++ 真的2/真的2/真的2.cpp
@@ -0,0 +1,16 @@
+#define  _CRT_SECURE_NO_WARNINGS
+#include<stdio.h>
+int main()
+{
+	int a = 0;
+	scanf("%d", &a);
+	if (a > 1)
+	{
+		printf("I Love China");
+	}
+	else
+	{
+		printf("ÎÒ°®ÖÐ¹ú");
+	}
+	return 0;
+}
